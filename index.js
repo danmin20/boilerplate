7bdb8c5a2e9fa57ae66b19749007fc45fde83bdf
leejeongmin <danmin20@khu.ac.kr>
2020-05-06 00:18:44 +0900

    bodyparser

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ const express = require("express");
 const app = express();
 const port = 5000;
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+// application/json
+app.use(bodyParser.json());
+
+const { User } = require("./models/User");
+
 mongoose
   .connect(
     "mongodb+srv://danmin:1108@cluster0-ofdek.mongodb.net/test?retryWrites=true&w=majority",
@@ -16,4 +25,12 @@ mongoose
   .catch((err) => console.log(err));
 
 app.get("/", (req, res) => res.send("hello world"));
+app.post("/register", (req, res) => {
+  const user = new User(req.body);
+  user.save((err, doc) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({ success: true });
+  });
+});
+
 app.listen(port, () => console.log(`Example app listening on port ${port}`));
