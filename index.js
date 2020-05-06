5e72674995c43dcecfd8ec333db9224a10127d17
leejeongmin <danmin20@khu.ac.kr>
2020-05-06 16:09:26 +0900

    password hashing

--- index.js
+++ index.js
@@ -25,10 +25,6 @@ mongoose
 app.get("/", (req, res) => res.send("hello world"));
 app.post("/register", (req, res) => {
   const user = new User(req.body);
-
-  // 비밀번호 암호화
-  
-
   user.save((err, doc) => {
     if (err) return res.json({ success: false, err });
     return res.status(200).json({ success: true });
